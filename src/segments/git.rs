704446e3190a4f2f0de00f6d3369687860b28405
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-10-17 01:57:30 +0900

    Clean up the git segment

--- src/segments/git.rs
+++ src/segments/git.rs
@@ -1,90 +1,117 @@
-use git2::{Oid, Repository, Status, StatusOptions};
+use git2::{BranchType, Oid, Reference, Repository, Status, StatusOptions};
 use std::io;
 
 use crate::prompt::{Prompt, PromptError};
 
-#[derive(Debug)]
-pub enum HeadStatus {
-    Branch(String),
-    Tag(String),
-    Commit(Oid),
-    Unknown,
-}
-
-fn find_tag(repo: &Repository, oid: &Oid) -> Option<Option<String>> {
-    let tag = repo
-        .references()
+fn find_tag<'repo>(repo: &'repo Repository, oid: &Oid) -> Option<Reference<'repo>> {
+    repo.references()
         .ok()?
         .flatten()
         .filter(|r| r.is_tag())
         .filter(|r| r.target().as_ref() == Some(oid))
-        .next()?;
-
-    Some(tag.shorthand().map(|name| name.to_string()))
+        .next()
 }
 
-pub fn get_head_status(repo: &Repository) -> Option<HeadStatus> {
-    let default_ref_name = "???";
-    let head = repo.head().ok()?;
-
-    if head.is_branch() {
-        // HEAD is a branch
-        let branch_name = head.shorthand().unwrap_or(default_ref_name).to_string();
-
-        Some(HeadStatus::Branch(branch_name))
-    } else if let Some(oid) = head.target() {
-        if let Some(tag_name) = find_tag(repo, &oid) {
-            // HEAD is a tag
-            let tag_name = tag_name.unwrap_or(default_ref_name.to_string());
-
-            Some(HeadStatus::Tag(tag_name))
-        } else {
-            // HEAD is a commit
-            Some(HeadStatus::Commit(oid))
-        }
-    } else {
-        Some(HeadStatus::Unknown)
-    }
-}
-
-pub fn prompt_segment<W: io::Write>(p: &mut Prompt<W>) -> Result<(), PromptError> {
-    // Open the current repository
-    let repo = match Repository::discover(".") {
-        Ok(repo) => repo,
-        Err(_) => return Ok(()),
-    };
-
-    // Show the HEAD status
+fn format_head_status(repo: &Repository, head: &Option<Reference>) -> String {
     let branch_icon = "\u{f418}";
     let tag_icon = "\u{f412}";
     let hash_len = 6;
     let commit_icon = "\u{f417}";
 
-    let head_text = match get_head_status(&repo) {
-        Some(HeadStatus::Branch(name)) => format!("{} {}", branch_icon, name),
-        Some(HeadStatus::Tag(name)) => format!("{} {}", tag_icon, name),
-        Some(HeadStatus::Commit(oid)) => {
-            let mut hash_str = oid.to_string();
-            hash_str.truncate(hash_len);
-            format!("{} {}", commit_icon, hash_str)
+    let head = match head {
+        Some(head) => head,
+
+        // Empty repository
+        None => {
+            return format!("{} {}", branch_icon, "master");
         }
-        Some(HeadStatus::Unknown) | None => "?".to_string(),
     };
 
-    // Get statuses
-    let mut file_statuses = Status::empty();
+    if head.is_branch() {
+        // HEAD is a branch
+        let branch_name = head.shorthand().unwrap_or("?");
 
-    if let Ok(statuses) = repo.statuses(Some(StatusOptions::new().include_untracked(true))) {
-        for status in statuses.iter() {
-            file_statuses.insert(status.status());
-
-            if file_statuses.is_all() {
-                break;
-            }
-        }
+        return format!("{} {}", branch_icon, branch_name);
     }
 
-    // Set colors
+    // Get the commit hash of HEAD
+    let oid = match head.target() {
+        Some(oid) => oid,
+
+        // Because WTF
+        None => {
+            return format!("{} {}", branch_icon, "!");
+        }
+    };
+
+    if let Some(tag) = find_tag(repo, &oid) {
+        // HEAD is a tag
+        let tag_name = tag.shorthand().unwrap_or("?");
+
+        return format!("{} {}", tag_icon, tag_name);
+    } else {
+        // HEAD is a commit
+        let mut hash_str = oid.to_string();
+        hash_str.truncate(hash_len);
+
+        return format!("{} {}", commit_icon, hash_str);
+    }
+}
+
+fn get_repository_statuses(repo: &Repository) -> Status {
+    let mut status_options = StatusOptions::new();
+    status_options.include_untracked(true);
+
+    repo.statuses(Some(&mut status_options))
+        .map(|statuses| statuses.iter().fold(Status::empty(), |a, b| a | b.status()))
+        .unwrap_or(Status::empty())
+}
+
+fn format_status_icons(repo_statuses: &Status) -> Option<String> {
+    let modified_statuses = Status::INDEX_MODIFIED
+        | Status::INDEX_RENAMED
+        | Status::INDEX_TYPECHANGE
+        | Status::WT_MODIFIED
+        | Status::WT_RENAMED
+        | Status::WT_TYPECHANGE;
+    let added_statuses = Status::INDEX_NEW | Status::WT_NEW;
+    let deleted_statuses = Status::INDEX_DELETED | Status::WT_DELETED;
+    let conflicted_statuses = Status::CONFLICTED;
+
+    let modified_icon = "…";
+    let plus_minus_icon = "±";
+    let plus_icon = "+";
+    let minus_icon = "-";
+    let conflict_icon = "\u{f47f}";
+
+    let mut status_icons = String::new();
+
+    if repo_statuses.intersects(modified_statuses) {
+        status_icons += modified_icon;
+    }
+
+    match (
+        repo_statuses.intersects(added_statuses),
+        repo_statuses.intersects(deleted_statuses),
+    ) {
+        (true, true) => status_icons += plus_minus_icon,
+        (true, false) => status_icons += plus_icon,
+        (false, true) => status_icons += minus_icon,
+        (false, false) => {}
+    }
+
+    if repo_statuses.intersects(conflicted_statuses) {
+        status_icons += conflict_icon;
+    }
+
+    if status_icons.is_empty() {
+        None
+    } else {
+        Some(status_icons)
+    }
+}
+
+fn get_segment_colors(repo_statuses: &Status) -> (&'static str, &'static str) {
     let conflicted_statuses = Status::CONFLICTED;
 
     let unstaged_statuses = Status::WT_NEW
@@ -99,67 +126,77 @@ pub fn prompt_segment<W: io::Write>(p: &mut Prompt<W>) -> Result<(), PromptError
         | Status::INDEX_RENAMED
         | Status::INDEX_TYPECHANGE;
 
-    let background;
-    let foreground;
-
-    if file_statuses.intersects(conflicted_statuses) {
-        background = "red";
-        foreground = "black";
-    } else if file_statuses.intersects(unstaged_statuses) {
-        background = "yellow";
-        foreground = "black";
-    } else if file_statuses.intersects(staged_statuses) {
-        background = "green";
-        foreground = "black";
+    if repo_statuses.intersects(conflicted_statuses) {
+        ("red", "black")
+    } else if repo_statuses.intersects(unstaged_statuses) {
+        ("yellow", "black")
+    } else if repo_statuses.intersects(staged_statuses) {
+        ("green", "black")
     } else {
-        background = "green";
-        foreground = "black";
+        ("green", "black")
     }
+}
 
-    // Get status icons
-    let modified_statuses = Status::INDEX_MODIFIED
-        | Status::INDEX_RENAMED
-        | Status::INDEX_TYPECHANGE
-        | Status::WT_MODIFIED
-        | Status::WT_RENAMED
-        | Status::WT_TYPECHANGE;
-    let added_statuses = Status::INDEX_NEW | Status::WT_NEW;
-    let deleted_statuses = Status::INDEX_DELETED | Status::WT_DELETED;
+fn format_graph(repo: &Repository, head: &Option<Reference>) -> Option<String> {
+    let head = head.as_ref()?;
 
-    let modified_icon = "…";
-    let plus_minus_icon = "±";
-    let plus_icon = "+";
-    let minus_icon = "-";
-    let conflict_icon = "\u{f47f}";
+    let ahead_icon = "\u{f176}";
+    let behind_icon = "\u{f175}";
 
-    let mut status_icons = String::new();
+    // Find the local branch
+    let local_branch = repo
+        .find_branch(head.shorthand()?, BranchType::Local)
+        .ok()?;
+    let upstream_branch = local_branch.upstream().ok()?;
 
-    if file_statuses.intersects(modified_statuses) {
-        status_icons += modified_icon;
+    let head_oid = head.target()?;
+    let upstream_oid = upstream_branch.get().target()?;
+
+    let (ahead, behind) = repo.graph_ahead_behind(head_oid, upstream_oid).ok()?;
+
+    match (ahead, behind) {
+        (0, 0) => None,
+        (ahead, 0) => Some(format!("{}{}", ahead_icon, ahead)),
+        (0, behind) => Some(format!("{}{}", behind_icon, behind)),
+        (ahead, behind) => Some(format!("{}{}{}{}", ahead_icon, ahead, behind_icon, behind)),
     }
+}
 
-    match (
-        file_statuses.intersects(added_statuses),
-        file_statuses.intersects(deleted_statuses),
-    ) {
-        (true, true) => status_icons += plus_minus_icon,
-        (true, false) => status_icons += plus_icon,
-        (false, true) => status_icons += minus_icon,
-        (false, false) => {}
-    }
+pub fn prompt_segment<W: io::Write>(p: &mut Prompt<W>) -> Result<(), PromptError> {
+    // Open the current repository
+    let repo = match Repository::discover(".") {
+        Ok(repo) => repo,
+        Err(_) => return Ok(()),
+    };
 
-    if file_statuses.intersects(conflicted_statuses) {
-        status_icons += conflict_icon;
-    }
+    let head = repo.head().ok();
 
-    // Show the git segment
-    let mut text = String::new();
+    // Show the HEAD status
+    let head_status = format_head_status(&repo, &head);
 
-    text += &head_text;
+    // Get statuses
+    let repo_statuses = get_repository_statuses(&repo);
 
-    if !status_icons.is_empty() {
+    // Show the status icons
+    let status_icons = format_status_icons(&repo_statuses);
+
+    // Get the segment colors
+    let (background, foreground) = get_segment_colors(&repo_statuses);
+
+    // Show the graph
+    let graph = format_graph(&repo, &head);
+
+    // Build the git segment
+    let mut text = head_status;
+
+    if let Some(status_icons) = status_icons {
         text += " ";
         text += &status_icons;
+    }
+
+    if let Some(graph) = graph {
+        text += " ";
+        text += &graph;
     }
 
     p.write_segment(background, foreground, &text)?;
