9e5bc34e4b97f6953f30682eb483516a3d07ed83
Ryooooooga <eial5q265e5@gmail.com>
2021-09-28 20:49:09 +0900

    Display minutes and hours in duration segment

--- src/segments/duration.rs
+++ src/segments/duration.rs
@@ -5,6 +5,8 @@ fn render_duration(duration: f64) -> String {
     static MICRO_SEC: f64 = 1e-6;
     static MILLI_SEC: f64 = 1e-3;
     static SEC: f64 = 1.0;
+    static MIN: f64 = 60.0 * SEC;
+    static HOUR: f64 = 60.0 * MIN;
 
     if duration < MILLI_SEC {
         format!("{:.0}μs", duration / MICRO_SEC)
@@ -16,10 +18,19 @@ fn render_duration(duration: f64) -> String {
         format!("{:.0}ms", duration / MILLI_SEC)
     } else if duration < 10.0 * SEC {
         format!("{:.2}s", duration / SEC)
-    } else if duration < 100.0 * SEC {
+    } else if duration + 0.05 * SEC < MIN {
         format!("{:.1}s", duration / SEC)
+    } else if duration + 0.5 * SEC < HOUR {
+        let d = duration + 0.5 * SEC;
+        let mins = (d / MIN).floor();
+        let secs = ((d % MIN) / SEC).floor();
+        format!("{:.0}m {:.0}s", mins, secs)
     } else {
-        format!("{:.0}s", duration)
+        let d = duration + 0.5 * SEC;
+        let hours = (d / HOUR).floor();
+        let mins = ((d % HOUR) / MIN).floor();
+        let secs = ((d % MIN) / SEC).floor();
+        format!("{:.0}h {:.0}m {:.0}s", hours, mins, secs)
     }
 }
 
@@ -48,9 +59,21 @@ fn test_render_duration() {
     assert_eq!(render_duration(12.000_000_000), "12.0s");
     assert_eq!(render_duration(12.345_678_900), "12.3s");
 
-    assert_eq!(render_duration(100.000_000), "100s");
-    assert_eq!(render_duration(120.000_000), "120s");
-    assert_eq!(render_duration(123.456_789), "123s");
+    assert_eq!(render_duration(59.949), "59.9s");
+    assert_eq!(render_duration(59.950), "1m 0s");
+
+    assert_eq!(render_duration(100.000), "1m 40s");
+    assert_eq!(render_duration(119.500), "2m 0s");
+    assert_eq!(render_duration(120.000), "2m 0s");
+    assert_eq!(render_duration(123.456), "2m 3s");
+    assert_eq!(render_duration(133.456), "2m 13s");
+    assert_eq!(render_duration(1234.567), "20m 35s");
+
+    assert_eq!(render_duration(3599.499), "59m 59s");
+    assert_eq!(render_duration(3599.500), "1h 0m 0s");
+
+    assert_eq!(render_duration(3600.0), "1h 0m 0s");
+    assert_eq!(render_duration(3912.0), "1h 5m 12s");
 }
 
 pub fn build_segment<'ctx>(context: &'ctx Context) -> Option<Segment<'ctx>> {
